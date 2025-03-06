73ac28a42c8581e56b3b53a92bee992ef7ab7a31
Aleix Ortigosa <aleixortigosa205@gmail.com>
2025-03-06 09:23:24 +0100

    Add files via upload

---
+++ Edge.java
@@ -0,0 +1,19 @@
+package javadoc;
+
+public class Edge {
+    private int weight;
+    private static int id1;
+    private static int id2;
+    //tipo tiene 3 valores: c, arista entre centro; s, arista entre sensores; x, arista centro-sensor
+    private static char tipo;
+
+    public Edge(int i1, int i2, char t) {
+        id1 = i1;
+        id2 = i2;
+        tipo = t;
+    }
+
+    public void setWeight(int w) {
+        weight = w;
+    }
+}
---
+++ Graph.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Graph {
+    private static Sensores Snodes;
+    private static CentrosDatos Cnodes;
+    private ArrayList<Edge> adjs;
+    public Graph()
+}
