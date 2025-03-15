5a3018fcd2918895198b7cf42a869929fab45971
Aleix Ortigosa <aleixortigosa205@gmail.com>
2025-03-15 17:46:53 +0100

    Structural change
    Very beneficial tradeoff, all operator deciders and doers have at most linear cost and the code is a lot cleaner. Only adjusting the randomnes of values remains

--- Graph.java
+++ Graph.java
@@ -4,67 +4,70 @@ import java.util.*;
 import static java.lang.Math.min;
 import static java.lang.Math.random;
 
+//UN SENSOR SOLO SACA UNA ARISTA VERDAD??? MUY IMPORTANTE
 public class Graph {
-    //Quiza va bien guardar el numero actual de aristas, la suma de tamaños de todos los arraylists
+    private int[] Scons;
+    private int[] Ccons;
     private int ssize;
     private int csize;
     private static Sensores Snodes;
     private static CentrosDatos Cnodes;
-    private HashMap<Integer, ArrayList<Edge>> adjs;  // Usamos HashMap para almacenar varias aristas por id2
+    private HashMap<Integer, ArrayList<Edge>> adjs;
 
     public Graph(Sensores s, CentrosDatos c) {
         Snodes = s;
         Cnodes = c;
-        adjs = new HashMap<>();  // Inicialización del HashMap
+        ssize = Snodes.size();
+        csize = Cnodes.size();
+        Scons = new int[ssize];
+        Ccons = new int[csize];
+        adjs = new HashMap<>();
     }
 
-
+    /*
     //easter egg, video top de e-k:https://www.youtube.com/watch?v=RppuJYwlcI8
     public void fillEdges() {
         // Añadimos las aristas entre nodos de sensores
         for (int i = 0; i < ssize; ++i) {
             for (int j = 0; j < ssize; ++j) {
                 if (i != j) {
-                    adjs.computeIfAbsent(j, k -> new ArrayList<>()).add(new Edge(i, j, 's'));  // Usamos computeIfAbsent para inicializar la lista si no existe
+                    Sadjs.computeIfAbsent(j, k -> new ArrayList<>()).add(new Edge(i, j, 's'));  // Usamos computeIfAbsent para inicializar la lista si no existe
                 }
             }
         }
         // Añadimos las aristas entre nodos de sensores y centros de datos
         for (int i = 0; i < ssize; ++i) {
             for (int j = 0; j < csize; ++j) {
-                adjs.computeIfAbsent(j, k -> new ArrayList<>()).add(new Edge(i, j, 'c'));
+                Cadjs.computeIfAbsent(j, k -> new ArrayList<>()).add(new Edge(i, j, 'c'));
             }
         }
 
         // Falta pensar cómo manejar los límites de 3 y 25, así como los pesos/capacidad
     }
+    */
 
+    //Nota: Mirar si el nextint no repetirá, hay que estar seguros
     public void simpleSolution() {
         Random myRandom = new Random();
-
         // Añadimos aristas entre sensores y centros de datos, con un límite de 25 conexiones por centro de datos
         for (int i = 0; i < csize; ++i) {
             for (int j = 0; j < min(25, ssize); ++j) {
                 int next = myRandom.nextInt(ssize);
-                if (!limit3(next)) {
-                    adjs.computeIfAbsent(i, k -> new ArrayList<>()).add(new Edge(next, i, 'c'));
-                }
+                adjs.computeIfAbsent(next, k -> new ArrayList<>()).add(new Edge(next, i, 'c'));
+                ++Ccons[i];
             }
         }
 
+        //HAY QUE REHACER ESTA PARTE MAÑANA LO MIRO NO ME HA DADO TIEMPO 
         // Añadimos aristas entre sensores, con un límite de 3 conexiones por sensor
         for (int i = 0; i < ssize; ++i) {
             for (int j = 0; j < min(3, ssize - 1); ++j) {
                 int next = myRandom.nextInt(ssize);
-                if (i != next && !limit3(next) && !limit3(i)) {
-                    adjs.computeIfAbsent(next, k -> new ArrayList<>()).add(new Edge(i, next, 's'));
+                if (i != next && !adjs.containsKey(i) && Scons[next] < 3) {
+                    adjs.computeIfAbsent(i, k -> new ArrayList<>()).add(new Edge(i, next, 's'));
                 }
             }
         }
-    }
-    public boolean limit3(int id2){
-        // Verificamos si existe la clave en el HashMap y si la lista asociada tiene más de 3 aristas
-        return adjs.containsKey(id2) && adjs.get(id2).size() > 3;
     }
 
     //Operadores:
@@ -74,101 +77,117 @@ public class Graph {
  //- eliminar conexión
 
 
-    //AUXILIARES PARA OPERADORES
     public boolean ableErase() {
         return !adjs.isEmpty();
     }
-    //solo por probar borro la arista con mayor coste, sino tambien podemos borrar una random hay que experimentar
+
     public void eraseEdge() {
         Random r = new Random();
-        int i = r.nextInt(ssize);//otra vez el problema de la adjacency list
+        int i = r.nextInt(sz);
         while (true) {
-            if (adjs.containsKey(i)) break;
+            if (adjs.get(i) == null) break;
+            i = r.nextInt(sz);
+        }
+        int id = adjs.get(i).getId2;
+        char t = adj.get(i).getTipo;
+        adjs.put(i,null);
+        if (t == 's') --Scons[id];
+        else --Ccons[id];
+    }
+
+    //MUY IMPORTANTE:Nunca llegaremos a tener 3*ssize+25*csize porque solo sale una edge de cada sensor
+    //haria una variable total correspondiente a las casillas del hash no nulas y luego
+    //return total < ssize;
+    //es abusar de atributos? tu decides
+    public boolean ableAdd() {
+        for (int i = 0; i < ssize; ++i) {
+            if (adjs.get(i) == null) return true;
+        }
+        return false;
+    }
+
+    public void addEdge() {
+        Random r = new Random();
+        int i = r.nextInt(ssize);
+        while (true) {
+            if (adjs.get(i) == null) break;
             i = r.nextInt(ssize);
         }
-        int j = r.nextInt(adjs.get(i).size());
-        adjs.get(i).remove(j);
-        if (adjs.get(i).isEmpty()) adjs.remove(i);
+        //As we know we are able to add one, we just try with certainty across all the nodes
+        for (int j = 0; j < csize; ++j) if (Ccons[j] < 25) {adjs.put(i,new Edge(i,j,'c')); return;}
+        for (int j = 0; j < ssize; ++j) if (Ccons[j] < 3) {adjs.put(i,new Edge(i,j,'s')); return;}
     }
 
-    public boolean ableAdd() {
-        return adjs.size() < ssize*3 + csize*25;
-    }
 
-    //Pondre lo de dentro de if else en otra funcion auxiliar por legibilidad seguramente
-    public void addEdge() {
-        while (true) {
-            int rand = (int) random();
-            int aux = rand % 2;
-            char mode;
-            if (aux == 0) rand %= ssize;
-            else rand %= csize;
-            //rand %= size de uno de los 2 pero primero debemos hablar lo de la adjlist si ves este push hoy tranqui mañana lo entenderas pero mejor hablarlo en persona
-            if (adjs.containsKey(rand) && adjs.get(rand).size() < 3) {
-                int srand = (int) random();
-                while (true) {
-                    srand %= ssize; //da igual que rand(el primer indice) sea s o c, el siguiente siempre es s
-                    if (srand != rand) break;
-                }
-                if (aux == 0) adjs.get(rand).add(new Edge(srand, rand, 's'));
-                else adjs.get(rand).add(new Edge(srand, rand, 'c'));
-                //añadir arista, puedo coger una a s rand o a c rand pero hay q hablar conds
-                return;
+    public boolean ableSwitchOrigin() {
+        if (adjs.isEmpty()) return false;
+        bool hueco = false;
+        bool arista = false;
+        for (int i = 0; i < ssize; ++i) {
+            if (!hueco && adjs.get(i) == null) {
+                if (arista) return true;
+                else hueco = true;
             }
-            else if(!adjs.containsKey(rand)) {
-                adjs.put(rand,new ArrayList<Edge>());
-                int srand;
-                while (true) {
-                    srand = (int) random();
-                    srand %= ssize; //da igual que rand(el primer indice) sea s o c, el siguiente siempre es s
-                    if (srand != rand) break;
-                }
-                if (aux == 0) adjs.get(rand).add(new Edge(srand, rand, 's'));
-                else adjs.get(rand).add(new Edge(srand, rand, 'c'));
-                return;
-                //ídem
+            if (!arista && adjs.get(i) != null) {
+                if (hueco) return true;
+                else arista = true;
             }
         }
-    }
-
-
-    public bool ableSwitch() {
-        return true;
-        //tan costoso de mirar que hace que este operador no valga la pena, pendiente discutir
+        return false;
     }
 
     //en este caso no hace falta hacer erase y luego add, asi es mas eficiente
     public void switchOrigin() {
         Random r = new Random();
-        int i = r.nextInt(ssize);//otra vez el problema de la adjacency list
-        while (true) {
-            if (adjs.containsKey(i)) break;
-            i = r.nextInt(ssize);
+        int i = r.nextInt(ssize);
+        int j = i;
+        boolean foundi = false;
+        boolean foundj = false;
+        while (!foundi && !foundj) {
+            if (!foundi && adjs.get(i) == null) foundi = true;
+            else if (!foundi) i = r.nextInt(ssize);
+            if (!foundj && adjs.get(j) != null) foundj = true;
+            else if (!foundj) j = r.nextInt(ssize);
         }
-        int j = r.nextInt(adjs.get(i).size());
-        adjs.get(i).get(j).setId1(4);
+        Edge aux = new Edge(i,adjs.get(j).getId2,adjs.get(j).getTipo);
+        adjs.put(i,aux);
+        adjs.put(j,null);
     }
 
-    private bool ableSwitchDestination() {
-        return true;
-        //lo malo es que hay que tener en cuenta las aristas del nodo al que le quitaremos una
-        //esto es porque hay que ver si nos queda capacidad para aristas sin contar el nodo destination previo 
+
+    private boolean ableSwitchDestination() {
+        if (adjs.isEmpty()) return false;
+        //Hay que mirar todo y ver si hay al menos 2 nodos con huecos!
+        //Porque si hay solo una cabe la posibilidad de que me toque hacer switch destination de esa misma y entonces sería como no hacer nada
+        int count = 0;
+        for (int i = 0; i < csize; ++i) {
+            if (Ccons[i] < 25) ++count;
+            if (count == 2) return true;
+        }
+        for (int i = 0; i < ssize; ++i) {
+            if (Scons[i] < 3) ++count;
+            if (count == 2) return true;
+        }
+        return false;
     }
 
     public void switchDestination() {
-        //if (ableSwitchDsetination)
-        //lo unico que s eme ocurre es hacer la comprobacion aqui, sera costoso...
         Random r = new Random();
-        int i = r.nextInt(ssize);//otra vez el problema de la adjacency list
-        while (true) {
-            if (adjs.containsKey(i)) break;
-            i = r.nextInt(ssize);
+        int i = r.nextInt(ssize);
+        int j = i;
+        boolean foundi = false;
+        boolean foundj = false;
+        char t;
+        while (!foundi && !foundj) {
+            if (!foundi && adjs.get(i) != null) foundi = true;
+            else if (!foundi) i = r.nextInt(ssize);
+            if (!foundj) {
+                if (Ccons[j] < 25) {foundj = true; t = 'c';}
+                else if (Scons[j] < 3) {foundj = true; t = 's';}
+            }
+            else if (!foundj) j = r.nextInt(ssize);
         }
-        int j = r.nextInt(adjs.get(i).size());
-        j = adjs.get(i).get(j).getId1();
-        //i = bucle hasta encontrar nuevo destino VÁLIDO
-        //eraseSpecificEdge(x),
-        //la i y la j seran el nuevo destino y el origen respectivamente
-        //addSpecificEdge(i,j), implementación trivial
+        Edge aux = new Edge(i,j,t);
+        adjs.put(i,aux);
     }
 }
