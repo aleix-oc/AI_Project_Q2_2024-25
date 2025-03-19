9c3c424329b7c8977004707f355ac33c58a4c307
Aleix Ortigosa <aleixortigosa205@gmail.com>
2025-03-19 17:29:28 +0100

    Main para experimentar con hill climb racing

--- Main.java
+++ Main.java
@@ -1,4 +1,12 @@
+//Input: Número de semilla
 public class Main {
     public static void main(String[] args) {
+        Sensores s = new Sensores(100,1234);
+        CentrosDatos c = new CentrosDatos(4,1234);
+        Graph g = new Graph(s,c);
+        Estado st = new Estado(g);
+        Problem problem = new Problem(st, new hillClimbingFinderSuccessorFunction)
+        Search search = new HillClimbingSearch();
+        SearchAgent agent = new SearchAgent(problem,search);
     }
 }
