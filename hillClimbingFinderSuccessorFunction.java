c40eb5fb18a1fffc610c58863c052e5a91b19dac
Aleix Ortigosa <aleixortigosa205@gmail.com>
2025-03-20 18:09:35 +0100

    FinderSuccessor

--- hillClimbingFinderSuccessorFunction.java
+++ hillClimbingFinderSuccessorFunction.java
@@ -7,21 +7,31 @@ public class hillClimbingFinderSuccessorFunction implements SuccessorFunction {
     public List getSuccessors(Object aState) {
         ArrayList sucesores= new ArrayList<>();
         Estado estado = (Estado) aState;
-        int sz = estado.getSsize();
+        int ss = estado.getSsize();
+        int cs = estado.getCsize();
         for (int id1 = 0; id1 < sz; ++id1) {
             for (int id2 = 0; id2 < sz; ++id2) {
                 if (id1 != id2) sucesores.add(new Successor("E",new Estado(estado.getRepresentacion()).switchEdges(id1,id2)));
             }
         }
-        int ss = estado.getSsize();
-        int cs = estado.getCsize();
         for (int id1 = 0; id1 < ss; ++id1) {
             for (int id2 = 0; id2 < ss; ++id2) {
-                if (estado.ableSwitch(id1,id2,'s')) sucesores.add(new Succesor("D",new Estado(estado.getRepresentacion()).switchDestination(id1,id2,'s')));
+                if (estado.ableSwitch(id1,id2,'s')) {
+                    Estado aux = new Estado(estado.getRepresentacion());
+                    if (aux.switchDestination(id1,id2,'s')) sucesores.add(new Succesor("D",aux));
+                }
             }
             for (int id2 = 0; id2 < cs; ++id2) {
-                if (estado.ableSwitch(id1,id2,'c')) sucesores.add(new Succesor("D",new Estado(estado.getRepresentacion()).switchDestination(id1,id2,'c')));
+                if (estado.ableSwitch(id1,id2,'c')) {
+                    Estado aux = new Estado(estado.getRepresentacion());
+                    if (aux.switchDestination(id1,id2,'c')) sucesores.add(new Succesor("D",aux));
+                }
             }
+        }
+        for (int id = 0; id < ss; ++id) {
+            Estado aux = new Estado(estado.getRepresentacion());
+            Edge e = aux.getEdge(id);
+            if (aux.jump(id)) sucesores.add(new Succesor("J",aux));
         }
         return sucesores;
     }
