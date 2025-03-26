05163bd8889c51ab118e8ef6877b772b757fbbd7
David Castro Paniello <davidcastropaniello@gmail.com>
2025-03-26 13:31:00 +0100

    Add files via upload

--- Estado.java
+++ Estado.java
@@ -28,15 +28,15 @@ public class Estado {
     public boolean jump(int id) { return representacion.jump(id); }
 
     public double heuristica() {
-       // double r = 0;
-        //int caps = 0;
-        //for(int i=0; i<representacion.getSsize(); ++i){
-          //  caps += representacion.getSnodes().get(i).getCapacidad();
-        //    if(representacion.getEdge(i).getVolumenFalso() < (int) representacion.getSnodes().get(i).getCapacidad() || representacion.getEdge(i).getVolumenReal()<0 || representacion.getEdge(i).getVolumenFalso() > 3*(int) representacion.getSnodes().get(i).getCapacidad() || representacion.getEdge(i).getVolumenReal() > 3*(int) representacion.getSnodes().get(i).getCapacidad()) r = Double.MAX_VALUE;
-        //}
-        //if(representacion.getVolumen() > caps) r = Double.MAX_VALUE;
-        //if(r==0) r = representacion.getCoste() -  representacion.getSsize()* representacion.getVolumen() * representacion.getVolumen();
-        return representacion.getCoste() - representacion.getSsize() * representacion.getVolumen() * representacion.getVolumen() * representacion.getVolumen();
+        double r = 0;
+        int caps = 0;
+        for(int i=0; i<representacion.getSsize(); ++i){
+            caps += representacion.getSnodes().get(i).getCapacidad();
+            if(representacion.getEdge(i).getVolumenFalso() < (int) representacion.getSnodes().get(i).getCapacidad() || representacion.getEdge(i).getVolumenReal()<0 || representacion.getEdge(i).getVolumenFalso() > 3*(int) representacion.getSnodes().get(i).getCapacidad() || representacion.getEdge(i).getVolumenReal() > 3*(int) representacion.getSnodes().get(i).getCapacidad()) r = Double.MAX_VALUE;
+        }
+        if(representacion.getVolumen() > caps) r = Double.MAX_VALUE;
+        if(r==0) r = representacion.getCoste() - 2 * representacion.getVolumen() * representacion.getVolumen();
+        return r;
     } //habrá que implementar funciones privadas de calculo de coste y datos aunque sea en grafo.
 
     public int getSsize() { return representacion.getSsize();}
