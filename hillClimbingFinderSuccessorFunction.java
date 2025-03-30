7d3a6b5e5fe8bc9bbfaaf818965a9c8229c7fbc7
David Castro Paniello <davidcastropaniello@gmail.com>
2025-03-30 15:01:15 +0200

    Update hillClimbingFinderSuccessorFunction.java

--- hillClimbingFinderSuccessorFunction.java
+++ hillClimbingFinderSuccessorFunction.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class hillClimbingFinderSuccessorFunction implements SuccessorFunction {
     public List getSuccessors(Object aState) {
-        ArrayList<String> sucesores= new ArrayList<>();//FALTABA STRING! Soy retra
+        ArrayList<String> sucesores= new ArrayList<>();
         Estado estado = (Estado) aState;
         int ss = estado.getSsize();
         int cs = estado.getCsize();
